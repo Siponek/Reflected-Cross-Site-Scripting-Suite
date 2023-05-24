3fb519fef4d3a27cca9656ad780505a461f88fe5
Siponek <szinkowicz@gmail.com>
2023-05-24 17:51:38 +0200

    Added default preparing for different steps

---
+++ features/steps/session-prepare.py
@@ -0,0 +1,41 @@
+# session-prepare.py
+from behave import * # pylint: disable-all
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+from requests import Session
+from enum import Enum
+
+class RequestType(Enum):
+    GET = 1
+    POST = 2
+    
+
+@given('I am a user accessing the "{page}" page with JS disabled')
+def step_user_page_no_js(context, page:str) -> None:
+    context.page = page
+    context.session = Session()
+    
+
+@when('I send "{content}" into query parameter "{query_param}"')
+def step_send_into_query_param(context, content:str, query_param :str) -> None:
+    # Prepare the url
+    url = f"http://localhost:8000/{context.page}?{query_param}={content}"
+    # Open the URL
+    context.response = context.session.get(url)
+    
+# Given I am a user accessing the "echo-name.php" page
+@given('I am a user accessing the "{page}" page')
+def step_given_i_am_a_user(context,page : str) -> None:
+    context.page = page
+    # context.driver = webdriver.Firefox()
+    context.driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    # context.driver.implicitly_wait(4)
+
+# I send '{payload}' into query parameter "name" as XSS
+@when("I send \'{payload}\' into query parameter \"{query_param}\" as XSS")
+def step_send_payload(context, payload:str, query_param:str) -> None:
+    # Prepare the url
+    url = f"http://localhost:8000/{context.page}?{query_param}={payload}"
+    # Open the URL
+    context.driver.get(url)
+    
