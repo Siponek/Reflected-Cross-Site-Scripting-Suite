50ac566efa6173fbd5089cc66e66cc73e976a2b5
Siponek <szinkowicz@gmail.com>
2023-05-24 17:47:46 +0200

    Finished echo-name scenarios

--- features/echo-name.feature
+++ features/echo-name.feature
@@ -1,10 +1,22 @@
-Feature: Welcome page
+Feature: echo-name.php Welcome Message
     As a user
     I want to see a welcome message with my name
     When I provide my name in the query parameter
 
+    Background:
+        Given I am a user accessing the "echo-name.php" page with JS disabled
+
     Scenario: Provide name in query parameter
-        Given I am a user
-        And I will access "echo-name.php" page
         When I send "John" into query parameter "name"
         Then I should see "Welcome John!"
+
+    Scenario: Attempt to pass XSS non-interactive payload in query parameter
+        When I send "John" into query parameter "name"
+        Then I should see "Welcome John!"
+        #? Test only when basic functionality works so no time is wasted on selenium
+        Given I am a user accessing the "echo-name.php" page
+        When I send '<style>@keyframes slidein {}</style><svg style="animation-duration:1s;animation-name:slidein;animation-iteration-count:2" onwebkitanimationiteration="alert(111)"></svg>' into query parameter "name" as XSS
+        Then I should not see "111" as an alert
+
+
+# <style>@keyframes slidein {}</style><script style=\"animation-duration:1s;animation-name:slidein;animation-iteration-count:2\" onwebkitanimationiteration=\"alert(\"ATTACK\")\"></script>
--- features/steps/echo-name.py
+++ features/steps/echo-name.py
@@ -1,15 +1,33 @@
-# in steps/welcome_steps.py
+# echo-name.py
 
 from behave import *
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoAlertPresentException
+from bs4 import BeautifulSoup
 
 
 @then('I should see "{text}"')
-def step_should_see(context, text):
+def step_should_see(context, text:str) -> None:
     # Check that the page body contains the expected text
-    body_text = context.driver.find_element(By.TAG_NAME, "body").text
-    assert text in body_text, f'Body text: "{body_text}" does not contain "{text}"'
+    soup : BeautifulSoup = BeautifulSoup(context.response.text, 'html.parser')
+    assert text in soup, f'Body text: "{soup}" does not contain "{text}"'
     
-    context.driver.quit()
+# I should not see "ATTACK" as an alert
+@then('I should not see "{text}" as an alert')
+def step_should_not_see_alert(context, text:str) -> None:
+    try:
+        # webdriver.support.ui.WebDriverWait(context.driver, 4).until(lambda driver: driver.switch_to.alert)
+        WebDriverWait(context.driver, 4).until(method=EC.alert_is_present(), message="Timed out waiting for alert")
+        alert = context.driver.switch_to.alert
+        alert_text : str = alert.text
+        assert text not in alert_text, f"Expected '{text}' not to be in alert, but found '{alert_text}'"
+        alert.dismiss()
+        
+        context.driver.quit()
+    except NoAlertPresentException:
+        pass
+    finally:
+        context.driver.quit()
+        
